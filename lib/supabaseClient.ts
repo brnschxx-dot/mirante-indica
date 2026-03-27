9f4adb3d6f5b0da6ea6d2d194704655a0a39f4b5
brnschxx-dot <brnschxx@gmail.com>
2026-03-27 14:27:37 -0300

    Corrigindo chave anon do Supabase

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://mhaqkbzhnzrxtvshijot.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_nxaXHKuHvhgxco_jceo9AA_F3aZrdt8'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_secret_VOrz4TTt-sAhnvVt43tEEw_04ForBcI'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
